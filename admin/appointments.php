f6f9b79832f76703f1078ceccf6d3c6ddfed8f94
RajazShah <azrishah1212@gmail.com>
2025-11-11 01:25:50 +0800

    tes

--- admin/appointments.php
+++ admin/appointments.php
@@ -14,12 +14,8 @@
                 <th>Appointment ID</th>
                 <th>User ID</th>
                 <th>Doctor</th>
-                <th>User ID</th>
-                <th>Doctor</th>
                 <th>Date</th>
                 <th>Time</th>
-                <th>Reason</th>
-                <th>Status</th>
                 <th>Reason</th>
                 <th>Status</th>
             </tr>
@@ -27,16 +23,6 @@
             <?php
             include "../db_connect.php";
 
-            $sql = "SELECT 
-                        appointment_id, 
-                        user_id, 
-                        doctor, 
-                        appointment_date AS date, 
-                        appointment_time AS time, 
-                        reason, 
-                        status 
-                    FROM customer_appointments";
-            
             $sql = "SELECT 
                         appointment_id, 
                         user_id, 
@@ -55,8 +41,8 @@
                     echo "<td>" . $row["appointment_id"] . "</td>";
                     echo "<td>" . $row["user_id"] . "</td>";  
                     echo "<td>" . $row["doctor"] . "</td>";
-                    echo "<td>" . $row["appointment_date"] . "</td>";
-                    echo "<td>" . $row["appointment_time"] . "</td>";
+                    echo "<td>" . $row["date"] . "</td>";
+                    echo "<td>" . $row["time"] . "</td>";
                     echo "<td>" . $row["reason"] . "</td>";
                     echo "<td>" . $row["status"] . "</td>";
                     echo "</tr>";
--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$dbname = "impian-optometrist";
+$dbname = "impianoptometrist";
 $port = 3307;
 
 $conn = mysqli_connect($host, $user, $pass, $dbname, $port);
